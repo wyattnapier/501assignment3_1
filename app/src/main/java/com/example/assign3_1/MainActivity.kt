348563e411a4b8e4823dab4bc32e439de7b8c2e6
Wyatt <wnapier@bu.edu>
2025-09-23 22:54:07 -0400

    using weight successfully to have 4 boxes

--- app/src/main/java/com/example/assign3_1/MainActivity.kt
+++ app/src/main/java/com/example/assign3_1/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.assign3_1.ui.theme.Assign3_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,10 +32,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Assign3_1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold( modifier = Modifier.fillMaxWidth() ) { innerPadding ->
+                    WeightModifiedRowScreen(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxWidth()
+                            .height(200.dp)
                     )
                 }
             }
@@ -31,17 +46,67 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WeightModifiedRowScreen(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .fillMaxWidth()
+    ) {
+        WeightModifiedColumn(
+            backgroundColor = Color.Red,
+            textInput = "Red - 25%",
+            modifier = Modifier.weight(25f)
+        )
+        WeightModifiedColumnWithChildren(
+            modifier = Modifier.weight(75f)
+        )
+
+    }
+}
+
+@Composable
+fun RowScope.WeightModifiedColumn(backgroundColor: Color, textInput: String, modifier: Modifier = Modifier) {
+    // modifier includes the weight
+    Box(modifier = modifier
+        .fillMaxHeight()
+        .background(backgroundColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = textInput,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun RowScope.WeightModifiedColumnWithChildren(modifier: Modifier = Modifier) {
+    // modifier includes the weight
+    Row(modifier = modifier
+        .fillMaxHeight()
+        .border(color = Color.Black, width = 2.dp),
+    ) {
+        WeightModifiedColumn(
+            backgroundColor = Color.LightGray,
+            textInput = "2f",
+            modifier = Modifier.weight(2f)
+        )
+        WeightModifiedColumn(
+            backgroundColor = Color.DarkGray,
+            textInput = "3f",
+            modifier = Modifier.weight(3f)
+        )
+        WeightModifiedColumn(
+            backgroundColor = Color.Blue,
+            textInput = "5f",
+            modifier = Modifier.weight(5f)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WeightModifiedRowScreenPreview() {
     Assign3_1Theme {
-        Greeting("Android")
+        WeightModifiedRowScreen(Modifier.fillMaxWidth().height(200.dp))
     }
 }
